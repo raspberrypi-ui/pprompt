82cb326265a763c4df16799c7c6ec5413d07b988
Simon Long <simon@raspberrypi.org>
2016-11-25 10:17:30 +0000

    Remove password change option; use Zenity to show a warning only.

--- sshpwd.sh
+++ sshpwd.sh
@@ -4,5 +4,5 @@ SALT=$(sudo grep pi /etc/shadow | sed -n 's/pi:\$6\$//;s/\$.*//p')
 HASH=$(mkpasswd -msha-512 raspberry "$SALT")
 
 if service ssh status | grep -q running && sudo grep -q "$HASH" /etc/shadow ; then
-	pprompt
+	zenity --warning --text="SSH is enabled and the default password for the 'pi' user has not been changed.\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password."
 fi
