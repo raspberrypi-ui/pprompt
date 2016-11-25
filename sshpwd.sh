b37a095218567232a6a90381b35bff5692eb5c42
Simon Long <simon@raspberrypi.org>
2016-11-25 15:39:24 +0000

    Updated routine for detecting default password to reduce logging trails.

--- sshpwd.sh
+++ sshpwd.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-SALT=$(sudo grep pi /etc/shadow | sed -n 's/pi:\$6\$//;s/\$.*//p')
-HASH=$(mkpasswd -msha-512 raspberry "$SALT")
+check_hash ()
+{
+   local SHADOW="$(sudo -n grep -E '^pi:' /etc/shadow 2>/dev/null)"
+   test -n "${SHADOW}" || return 0
+   local SALT=$(echo "${SHADOW}" | sed -n 's/pi:\$6\$//;s/\$.*//p')
+   local HASH=$(mkpasswd -msha-512 raspberry "$SALT")
 
-if service ssh status | grep -q running && sudo grep -q "$HASH" /etc/shadow ; then
+   if systemctl is-active ssh -q && echo "${SHADOW}" | grep -q "${HASH}"; then
 	zenity --warning --text="SSH is enabled and the default password for the 'pi' user has not been changed.\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password."
-fi
+   fi
+}
+
+check_hash
+unset check_hash
