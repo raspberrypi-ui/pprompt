9705a49d74352e0faa300dd00ae1dbf58434ac49
Simon Long <simon@raspberrypi.org>
2017-01-24 16:00:39 +0000

    Internationalisation added.

--- sshpwd.sh
+++ sshpwd.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-check_hash ()
-{
-   if ! id -u pi > /dev/null 2>&1 ; then return 0 ; fi
-   if grep -q "^PasswordAuthentication\s*no" /etc/ssh/sshd_config ; then return 0 ; fi
-   test -x /usr/bin/mkpasswd || return 0
-   SHADOW="$(sudo -n grep -E '^pi:' /etc/shadow 2>/dev/null)"
-   test -n "${SHADOW}" || return 0
-   if echo $SHADOW | grep -q "pi:!" ; then return 0 ; fi
-   SALT=$(echo "${SHADOW}" | sed -n 's/pi:\$6\$//;s/\$.*//p')
-   HASH=$(mkpasswd -msha-512 raspberry "$SALT")
-   test -n "${HASH}" || return 0
+export TEXTDOMAIN=pprompt
 
-   if echo "${SHADOW}" | grep -q "${HASH}"; then
-	zenity --warning --text="SSH is enabled and the default password for the 'pi' user has not been changed.\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password."
-   fi
-}
+. gettext.sh
 
-if service ssh status | grep -q running; then
-	check_hash
+if [ -e /home/.sshwarn ] ; then
+	zenity --warning --text="$(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password.")"
 fi
-unset check_hash
