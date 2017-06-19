258c995c4fa077fbafd7811c8b6d5289840b0617
Simon Long <simon@raspberrypi.org>
2017-06-19 13:22:28 +0100

    Add no-wrap parameter to workaround Stretch Zenity bug.

--- sshpwd.sh
+++ sshpwd.sh
@@ -5,5 +5,5 @@ export TEXTDOMAIN=pprompt
 . gettext.sh
 
 if [ -e /run/sshwarn ] ; then
-	zenity --warning --text="$(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password.")"
+	zenity --warning --no-wrap --text="$(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password.")"
 fi
