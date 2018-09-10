7038d21722456de3990d74f0e8f7fd40fe720721
Zander Brown <zbrown@gnome.org>
2018-09-10 20:32:21 +0100

    Reimplement in Python

--- sshpwd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export TEXTDOMAIN=pprompt
-
-. gettext.sh
-
-if [ -e /run/sshwarn ] ; then
-	zenity --warning --width=400 --text="$(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.\n\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password.")"
-fi
