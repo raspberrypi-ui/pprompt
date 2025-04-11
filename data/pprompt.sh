94f5ebcfb6b381f36c560f2ea7f7162423959b15
Simon Long <simon@raspberrypi.com>
2025-04-11 11:52:11 +0100

    Migrate to meson

---
+++ data/pprompt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export TEXTDOMAIN=pprompt
+
+. gettext.sh
+
+if [ -e /run/sshwarn ] ; then
+	zenoty --warning --width=400 --text="$(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.\n\nThis is a security risk - please login as the 'pi' user and run Raspberry Pi Configuration to set a new password.")"
+fi
